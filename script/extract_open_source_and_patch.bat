8e9fb8105ee14cc454bcd54a404eb96c2a2bcd94
KondeU <370242479@qq.com>
2021-06-26 21:40:12 +0800

    修改cppzmq一个丢失精度的转换报出的编译告警
    Desc: 修改cppzmq一个丢失精度的转换报出的编译告警，该转换根据上下文判断是正确和必要的
    Type: Feature
    Link: NA

--- script/extract_open_source_and_patch.bat
+++ script/extract_open_source_and_patch.bat
@@ -7,4 +7,7 @@ powershell -executionpolicy remotesigned -file "%~dp0extract_open_source.ps1"
 cd %OPEN_SOURCE_PATH%\zeromq
 %OPEN_SOURCE_PATH%\patch\bin\patch.exe -p1 < %PATCHES_PATH%\0001.patch
 
+cd %OPEN_SOURCE_PATH%\cppzmq
+%OPEN_SOURCE_PATH%\patch\bin\patch.exe -p1 < %PATCHES_PATH%\0002.patch
+
 cd %OPEN_SOURCE_PATH%
