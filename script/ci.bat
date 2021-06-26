063b694c4a149c1305d31f04f04a1f27bcb014ec
KondeU <370242479@qq.com>
2021-06-26 23:36:01 +0800

    ci for windows带上debug发布
    Desc: TiBus会生成自己的exe和dll，因此发布带上debug供使用者调试
    Type: Feature
    Link: NA

--- script/ci.bat
+++ script/ci.bat
@@ -3,16 +3,30 @@
 
 cd %~dp0
 call clean.bat
-mkdir ..\artifact\msvc_x64
-call build_msvc.bat Release x64 VS2015
-xcopy build\archive artifact\msvc_x64 /E /Y
+mkdir ..\artifact\msvc_x64_debug
+call build_msvc.bat Debug x64 VS2015
+xcopy build\archive artifact\msvc_x64_debug /E /Y
 rem build_msvc will change the working path to .\..
 
 cd %~dp0
 call clean.bat
-mkdir ..\artifact\msvc_x86
+mkdir ..\artifact\msvc_x64_release
+call build_msvc.bat Release x64 VS2015
+xcopy build\archive artifact\msvc_x64_release /E /Y
+rem build_msvc will change the working path to .\..
+
+cd %~dp0
+call clean.bat
+mkdir ..\artifact\msvc_x86_debug
+call build_msvc.bat Debug x86 VS2015
+xcopy build\archive artifact\msvc_x86_debug /E /Y
+rem build_msvc will change the working path to .\..
+
+cd %~dp0
+call clean.bat
+mkdir ..\artifact\msvc_x86_release
 call build_msvc.bat Release x86 VS2015
-xcopy build\archive artifact\msvc_x86 /E /Y
+xcopy build\archive artifact\msvc_x86_release /E /Y
 rem build_msvc will change the working path to .\..
 
 cd %~dp0
