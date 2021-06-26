1aa6252e9e8293b2690c2fc37d90dcbe0495fd5a
KondeU <370242479@qq.com>
2021-06-26 22:03:27 +0800

    本地开发调试使用的config
    Desc: 本地开发调试使用的config，用于开发配置构建环境，CMake的使用版本计划从该项目起由3.16.0升级为3.19.8
    Type: Feature
    Link: NA

---
+++ config.bat
@@ -0,0 +1,8 @@
+:: Manually configure the local environment.
+
+PATH=D:\CMake\cmake-3.16.0-win64-x64\bin;%PATH%
+PATH=D:\CMake\cmake-3.19.8-win64-x64\bin;%PATH%
+echo Env PATH: %PATH%
+
+:: The version of CMake finally used:
+cmake --version
