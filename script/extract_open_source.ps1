b14584ab185ee3b33567df15b2e2f27d14a33921
KondeU <370242479@qq.com>
2021-06-26 21:35:30 +0800

    修改脚本替换patch工具
    Desc: 修改脚本替换patch工具，加入if判断，如果解压过，就不再执行解压和打补丁
    Type: Feature
    Link: NA

--- script/extract_open_source.ps1
+++ script/extract_open_source.ps1
@@ -4,22 +4,28 @@ function Unzip([string]$sourceFile, [string]$targetFolder)
     {
         mkdir $targetFolder
     }
-	$sourceFile = Resolve-Path $sourceFile
-	$targetFolder = Resolve-Path $targetFolder
+    $sourceFile = Resolve-Path $sourceFile
+    $targetFolder = Resolve-Path $targetFolder
     $shellApp = New-Object -ComObject Shell.Application
     $zipFiles = $shellApp.NameSpace($sourceFile).Items()
     $shellApp.NameSpace($targetFolder).CopyHere($zipFiles)
 }
 
-Unzip "diffutils-2.8.7-1-bin.zip" "diff"
-Unzip "diffutils-2.8.7-1-dep.zip" "diff"
-Unzip "patch-2.5.9-7-bin.zip"     "patch"
-Unzip "zeromq-4.3.4.zip"          "."
-Unzip "cppzmq-4.7.1.zip"          "."
-Unzip "cereal-1.3.0.zip"          "."
-Unzip "msgpack-c-cpp-3.3.0.zip"   "."
+if(!(Test-Path "patch"))
+{
+    Unzip "patch-2.5.9-7-bin.zip"     "patch"
+    Unzip "diffutils-2.8.7-1-bin.zip" "diff"
+    Unzip "diffutils-2.8.7-1-dep.zip" "diff"
+    Unzip "zeromq-4.3.4.zip"          "."
+    Unzip "cppzmq-4.7.1.zip"          "."
+    Unzip "cereal-1.3.0.zip"          "."
+    Unzip "msgpack-c-cpp-3.3.0.zip"   "."
 
-Rename-Item "zeromq-4.3.4"        "zeromq"
-Rename-Item "cppzmq-4.7.1"        "cppzmq"
-Rename-Item "cereal-1.3.0"        "cereal"
-Rename-Item "msgpack-c-cpp-3.3.0" "msgpack"
+    Rename-Item "zeromq-4.3.4"        "zeromq"
+    Rename-Item "cppzmq-4.7.1"        "cppzmq"
+    Rename-Item "cereal-1.3.0"        "cereal"
+    Rename-Item "msgpack-c-cpp-3.3.0" "msgpack"
+
+    Remove-Item "patch\bin\patch.exe"
+    Unzip "patch-2.5.9_from_visual_studio_2017_strawberry_c_bin.zip" "patch\bin"
+}
