f950044e6e118102967dc4d7050d9ba2138cd6eb
KondeU <370242479@qq.com>
2021-06-26 21:37:31 +0800

    修改zeromq的构建文件，生成的库名称简化为libzmq
    Desc: 修改zeromq的构建文件，生成的库不带上版本等附加的多余名称后缀
    Type: Feature
    Link: NA

--- script/extract_open_source_and_patch.bat
+++ script/extract_open_source_and_patch.bat
@@ -1,5 +1,10 @@
-set PATCHES_PATH="%~dp0patches"
-set OPEN_SOURCE_PATH="%~dp0..\3rd"
+set PATCHES_PATH=%~dp0patches
+set OPEN_SOURCE_PATH=%~dp0..\3rd
 
 cd %OPEN_SOURCE_PATH%
 powershell -executionpolicy remotesigned -file "%~dp0extract_open_source.ps1"
+
+cd %OPEN_SOURCE_PATH%\zeromq
+%OPEN_SOURCE_PATH%\patch\bin\patch.exe -p1 < %PATCHES_PATH%\0001.patch
+
+cd %OPEN_SOURCE_PATH%
