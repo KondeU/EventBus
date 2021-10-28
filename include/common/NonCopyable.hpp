367369681019be28580c5b5f1cc957277a8a152b
KondeU <370242479@qq.com>
2021-10-28 22:12:58 +0800

    添加全局单例模式
    Desc: 添加全局单例模式，由于exe(out)和dll(so)间是不同的编译单元，因此单纯地通过static变量达不到全局的单例，引入一个动态链接库用来做全局单例的注册管理
    Type: Feature
    Link: NA

---
+++ include/common/NonCopyable.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace tibus {
+namespace common {
+
+class NonCopyable {
+protected:
+    NonCopyable() = default;
+    ~NonCopyable() = default;
+
+private:
+    NonCopyable(const NonCopyable&) = delete;
+    NonCopyable& operator=(const NonCopyable&) = delete;
+};
+
+}
+}
--- include/common/Singleton.hpp
+++ include/common/Singleton.hpp
@@ -1,34 +1,28 @@
 #pragma once
 
+#include "NonCopyable.hpp"
+
 namespace tibus {
 namespace common {
 
-// Singleton base class, allow to use C++ CRTP.
-template <typename T, int N = 0>
-// Allows to specify the value of N to achieve
-// a special scenario that there are different
-// instances of the same type, breaking the
-// singleton limit.
-class Singleton {
+template <typename T>
+class Singleton : public NonCopyable {
 public:
     static T& GetReference()
     {
         return instance;
     }
 
+protected:
+    Singleton() = default;
+    ~Singleton() = default;
+
 private:
     static T instance;
 };
 
-// If there is a class B that inherits class A,
-// and class A inherits from Singleton<A>, and
-// then an instance of B is created, then there
-// will be two instances of A, one for Singleton<A>
-// created in Singleton<A>::GetReference(), and
-// one is in the B instance. So the singleton
-// class is Singleton<A>, not A.
-template <typename T, int N>
-T Singleton<T, N>::instance;
+template <typename T>
+T Singleton<T>::instance;
 
 }
 }
