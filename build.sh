1a4998cee36b8955fa6c389bfa0ba1335680a141
Deu.K <370242479@qq.com>
2023-01-02 00:18:32 +0800

    feat: 支持Windows与Linux门禁构建

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -e
 CURRENT_PATH=$(cd "$(dirname "$0")"; pwd)
 
-cmake -S . -B build -A x64
+cmake -S . -B build
 cmake --build build --config Release --parallel
 cmake --install build --prefix output --config Release
